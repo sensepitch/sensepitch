8c37109e80f773c8fe7c103f997e6a8951676aad
Raid Thabet <thabet.raid123@gmail.com>
2026-08-19 10:49:37 +0100

    upgrade to Java 25 and update build dependencies
    - Switch CI and Maven compiler release to Java 25
    - Bump Lombok, ByteBuddy, maven-compiler-plugin, and Spotless plugin versions
    - Apply google-java-format 1.36.1 formatting changes

--- src/test/java/org/sensepitch/edge/BindAddressTest.java
+++ src/test/java/org/sensepitch/edge/BindAddressTest.java
@@ -12,27 +12,30 @@ class BindAddressTest {
   @Test
   @Timeout(value = 5, unit = TimeUnit.SECONDS)
   void invalidBindAddressThrows() {
-    var listenConfig = ListenConfig.builder()
-        .ssl(SslConfig.builder()
-            .keyPath("classpath:ssl/test.key")
-            .certPath("classpath:ssl/test.crt")
-            .build())
-        .address("not-a-valid-address")
-        .build();
+    var listenConfig =
+        ListenConfig.builder()
+            .ssl(
+                SslConfig.builder()
+                    .keyPath("classpath:ssl/test.key")
+                    .certPath("classpath:ssl/test.crt")
+                    .build())
+            .address("not-a-valid-address")
+            .build();
 
-    var config = ProxyConfig.builder()
-        .listen(listenConfig)
-        .sites(Map.of(
-            "example.com",
-            SiteConfig.builder()
-                .response(ResponseConfig.builder().text("demo").build())
-                .protection(ProtectionConfig.builder().disable(true).build())
-                .build()))
-        .metrics(MetricsConfig.builder().enable(false).build())
-        .build();
+    var config =
+        ProxyConfig.builder()
+            .listen(listenConfig)
+            .sites(
+                Map.of(
+                    "example.com",
+                    SiteConfig.builder()
+                        .response(ResponseConfig.builder().text("demo").build())
+                        .protection(ProtectionConfig.builder().disable(true).build())
+                        .build()))
+            .metrics(MetricsConfig.builder().enable(false).build())
+            .build();
 
     var proxy = new Proxy(config);
-    assertThatThrownBy(() -> proxy.start())
-        .isInstanceOf(Exception.class);
+    assertThatThrownBy(() -> proxy.start()).isInstanceOf(Exception.class);
   }
 }
